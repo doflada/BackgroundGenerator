c7e672c35b2683b37ea6553f7c44fbc3680fdd74
Dan Ilea <doflada@gmail.com>
2020-03-27 13:45:13 +0200

    ultimele modificari

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var without = require('lodash/without');
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log('raspuns:', without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
